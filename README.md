f35d7e3dc04b577a19e7383c357bbf04555f2afa
Sleighsoft <jpniedermeier@gmail.com>
2015-11-19 17:49:24 +0100

    Added userscript

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # cwtv-oops-remover
 Removes the annoying "Oops, you have an ad blocker enabled!  To watch this episode, please disable it and refresh the page." message and lets you watch the movie.
+
+Should work with tampermonkey for Chrome or greasemonkey for Firefox.
---
+++ cwtv-oops-remover.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         CWTV Add Warning Remover
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Kills the Oops
+// @author       You
+// @match        http://*/*
+// @grant        none
+// @include     http://*cwtv.com/*
+// @include     https://*cwtv.com/*
+// ==/UserScript==
+/* jshint -W097 */
+'use strict';
+
+window.eval("window.CWTVIsAdBlocking = function(){}");
